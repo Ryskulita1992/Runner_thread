b644cadce04df8d967d5aa1ce64cfe22761bb73d
admin <rsk.mva@gmail.com>
2020-02-17 18:54:06 +0600

    Runner 2 versions(edited)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,22 +5,39 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(" На старт, внимание марш!!!");
 
-        RunnerSec First = new RunnerSec(1);
-        First.start();
-        try {
-            First.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        RunnerSec Second = new RunnerSec(2);
-        Second.start();
-        RunnerSec Third = new RunnerSec(3);
-        Third.start();
-        RunnerSec Fourth = new RunnerSec(4);
-        Fourth.start();
-        RunnerSec Fifth = new RunnerSec(5);
+        Thread r1 = new Thread(new Runner());
+        Thread r2 = new Thread();
+        Thread r3 = new Thread();
+        Thread r4 = new Thread();
+        Thread r5 = new Thread();
 
+        r1.start();
+        r2.start();
+        r3.start();
+        r4.start();
+        r5.start();
 
     }
 }
 
+//
+//        Runner First = new Runner(1);
+//        First.start();
+//        try {
+//            First.join();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        Runner Second = new Runner(2);
+//
+//
+//
+//        Runner Third = new Runner(3);
+//
+//
+//        Runner Fourth = new Runner (4);
+//
+//    }
+//
+//    Runner Fifth = new Runner (5);
+//}
--- src/com/company/RunnerSec.java
+++ src/com/company/RunnerSec.java
@@ -1,41 +1,41 @@
-package com.company;
-
-public class RunnerSec  extends  Thread{
-    int id;
-
-    public RunnerSec(int id) {
-        this.id = id;}
-
-    public int getId(int id) {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-    @Override
-
-    public void run(){
-
-        int next=id+1;
-        try {
-            System.out.println("Runner " + getId(id) + " Берёт палочку и бежит к Runner " + next);
-            System.out.println("Runner 5 берет палочку бежит к финишу" );
-            sleep(1000);
-            System.out.println("Runner 5 бежит к Runner 4");
-            sleep(1000);
-        }catch (InterruptedException e){
-            System.out.println(" Прервано");
-        }try {
-            int previous=id-1;
-            System.out.println("Runner " + getId(id) + " Берёт палочку и бежит к Runner " + previous);
-            sleep(1000);
-        }catch (InterruptedException e){}
-
-      System.out.println("Финиш!!!");
-    }
-
-
-}
-
-
+//package com.company;
+//
+//public class RunnerSec  extends  Thread {
+//    int id;
+//
+//
+//    public RunnerSec(int id) {
+//        this.id = id;
+//    }
+//
+//    public int getId(int id) {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    @Override
+//
+//    public void run() {
+//        try {
+//            while (id == 5) {
+//
+//                int next = id + 1;
+//                System.out.println("Runner " + getId(id) + " Берёт палочку и бежит к Runner " + next);
+//                // sleep(1000);
+//                // int previous = id - 1;
+//                System.out.println("Runner 5 бежит к Runner 4");
+//                //System.out.println("Runner " + getId(id) + " Берёт палочку и бежит к Runner " + previous);
+//                System.out.println("Runner 5 берет палочку бежит к финишу");
+//            } //catch (InterruptedException i) {
+//            System.out.println(" ");
+//        }
+//    }
+//}
+//
+//
+//
+//
+//
